620f919424df3d6acedafa5c01ce1b78d239f8c8
DanielSed <danielsed@seznam.cz>
2024-11-06 00:34:15 +0100

    Add terraform for AKS deployment

---
+++ terraform/providers.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_version = ">=1.0"
+
+  required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~>1.5"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.9.1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "e3940e67-386e-4003-8682-1b67fe61d367"
+}
